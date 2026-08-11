1eed7eb66de0e277d4f556bff913d59b43f3d720
fuli.yew <fuli.yew@paynet.my>
2026-08-11 15:35:03 +0800

    rmeove unnecessary comments

--- main.go
+++ main.go
@@ -19,21 +19,6 @@ func main() {
 		log.Fatal("Config error: ", err)
 	}
 
-	/*from := mail.NewEmail(cfg.Email.FromName, cfg.Email.FromEmail)
-	to := mail.NewEmail("", "yewfuli41@gmail.com")
-	message := mail.NewSingleEmail(from, "SendGrid Test", to, "i love u", "<p>i love u</p>")
-
-	client := sendgrid.NewSendClient(cfg.Email.SendGridAPIKey)
-
-	resp, err := client.Send(message)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Status:", resp.StatusCode)
-	log.Println("Body:", resp.Body)*/
-
 	db, err := database.InitDB()
 	if err != nil {
 		log.Fatal("Database error: ", err)
